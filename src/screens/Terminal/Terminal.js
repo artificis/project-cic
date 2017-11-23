668075bc084684fb5802349fac6c3edfdd9827e2
DT <dark.turtle@protonmail.com>
2017-11-23 15:59:52 +0700

    Use autobind-decorator

--- src/screens/Terminal/Terminal.js
+++ src/screens/Terminal/Terminal.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import autobind from 'autobind-decorator';
 
 export default class Terminal extends React.Component {
+  @autobind
   handleTerminalClick() {
     this.promptInput.focus();
   }
 
   render() {
     return (
-      <div className="terminal" onClick={this.handleTerminalClick.bind(this)}>
+      <div className="terminal" onClick={this.handleTerminalClick}>
         <input className="terminal__input" type="text" ref={e => { this.promptInput = e; }} />
       </div>
     );
