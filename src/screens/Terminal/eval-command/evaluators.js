b9aaf5a3e861500d1d5405170291e4cf56571224
DT <dark.turtle@protonmail.com>
2017-11-24 15:07:22 +0700

    Redirect to gitlab oauth url on trying login

--- src/screens/Terminal/eval-command/evaluators.js
+++ src/screens/Terminal/eval-command/evaluators.js
@@ -1,11 +1,8 @@
+import { GITLAB_OAUTH_URL } from 'services/auth';
+
 async function login(argString, log) {
   log('Logging in...');
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      log('Logged in !');
-      resolve();
-    }, 3000);
-  });
+  setTimeout(() => window.location.assign(GITLAB_OAUTH_URL), 1000);
 }
 
 export {
--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,6 +1,7 @@
 import { createAction, handleActions } from 'redux-actions';
 
 // constants
+export const GITLAB_OAUTH_URL = `https://gitlab.com/oauth/authorize?client_id=${process.env.REACT_APP_GITLAB_APP_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_GITLAB_OAUTH_REDIRECT_URI}`;
 export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN';
 export const SET_OAUTH_TOKEN = 'SET_OAUTH_TOKEN';
 
