0814bcffde44485fff96a320799f6821860942cc
DT <dark.turtle@protonmail.com>
2017-11-25 04:54:26 +0700

    Add function export: `gitlabApiClient()`

--- src/services/logics.js
+++ src/services/logics.js
@@ -1,4 +1,7 @@
+import getGitLabApiClient from 'node-gitlab-api';
 import { logics as authLogics } from './auth';
+
+export const gitlabApiClient = oauthToken => getGitLabApiClient({ oauthToken });
 
 export default [
   ...authLogics
