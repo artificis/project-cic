575c177bf3c58ba61b74e35e5584758d649ce6c4
DT <dark.turtle@protonmail.com>
2017-11-24 15:19:07 +0700

    Change login log text

--- src/screens/Terminal/eval-command/evaluators.js
+++ src/screens/Terminal/eval-command/evaluators.js
@@ -1,7 +1,7 @@
 import { GITLAB_OAUTH_URL } from 'services/auth';
 
 async function login(argString, log) {
-  log('Logging in...');
+  log('Signing in...');
   setTimeout(() => window.location.assign(GITLAB_OAUTH_URL), 1000);
   return false;
 }
