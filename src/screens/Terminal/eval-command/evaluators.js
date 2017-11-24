05e7431d4c5bf7891fbcf9ff61244f1f05f39ec7
DT <dark.turtle@protonmail.com>
2017-11-24 16:10:23 +0700

    Check if already logged in

--- src/screens/Terminal/eval-command/evaluators.js
+++ src/screens/Terminal/eval-command/evaluators.js
@@ -1,9 +1,21 @@
-import { GITLAB_OAUTH_URL } from 'services/auth';
+import { createStructuredSelector } from 'reselect';
+import store from 'store';
+import { GITLAB_OAUTH_URL, authenticatedSelector } from 'services/auth';
+
+const selector = createStructuredSelector({
+  loggedIn: authenticatedSelector
+});
 
 async function login(argString, log) {
-  log('Signing in...');
-  setTimeout(() => window.location.assign(GITLAB_OAUTH_URL), 1000);
-  return false;
+  const { loggedIn } = selector(store.getState());
+  if (loggedIn) {
+    log('You are already signed in.');
+    return true;
+  } else {
+    log('Signing in...');
+    setTimeout(() => window.location.assign(GITLAB_OAUTH_URL), 1000);
+    return false;
+  }
 }
 
 export {
