597b618695a2c7c26eac300c4cb10b8d951cb33e
DT <dark.turtle@protonmail.com>
2017-11-25 09:59:50 +0700

    Implement command: `cd`

--- src/screens/Terminal/eval-command/evaluators.js
+++ src/screens/Terminal/eval-command/evaluators.js
@@ -1,17 +1,19 @@
 import { createStructuredSelector } from 'reselect';
 import store from 'store';
 import { GITLAB_OAUTH_URL, authenticatedSelector, currentUserSelector } from 'services/auth';
-import { getProjects } from 'services/repo';
+import { projectsSelector, getProjects, setCurrentProject } from 'services/repo';
 import { requiresAuth } from './decorators';
 
+const { getState, dispatch } = store;
 const selector = createStructuredSelector({
   loggedIn: authenticatedSelector,
-  user: currentUserSelector
+  user: currentUserSelector,
+  projects: projectsSelector
 });
 
 class Command {
   static login({ log }) {
-    const { loggedIn } = selector(store.getState());
+    const { loggedIn } = selector(getState());
     if (loggedIn) {
       log('You are already signed in.');
       return true;
@@ -24,19 +26,32 @@ class Command {
 
   @requiresAuth
   static whoami({ log }) {
-    const { user } = selector(store.getState());
+    const { user } = selector(getState());
     log(user.username);
     return true;
   }
 
   @requiresAuth
   static ls() {
-    store.dispatch(getProjects());
+    dispatch(getProjects());
+  }
+
+  @requiresAuth
+  static cd({ args, log }) {
+    const { projects } = selector(getState());
+    const project = projects.find(p => p.path === args[0]);
+    if (project) {
+      dispatch(setCurrentProject(project));
+    } else {
+      log(`cd: no such project or directory: ${args[0]}`);
+    }
+    return true;
   }
 }
 
 export default {
   login: Command.login,
   whoami: Command.whoami,
-  ls: Command.ls
+  ls: Command.ls,
+  cd: Command.cd
 };
--- src/services/repo/index.js
+++ src/services/repo/index.js
@@ -3,10 +3,12 @@ import { createAction, handleActions } from 'redux-actions';
 // constants
 export const GET_PROJECTS = 'GET_PROJECTS';
 export const SET_PROJECTS = 'SET_PROJECTS';
+export const SET_CURRENT_PROJECT = 'SET_CURRENT_PROJECT';
 
 // action creators
 export const getProjects = createAction(GET_PROJECTS);
 export const setProjects = createAction(SET_PROJECTS);
+export const setCurrentProject = createAction(SET_CURRENT_PROJECT);
 
 // reducer
 const initialState = {
@@ -20,8 +22,15 @@ export default handleActions({
   [SET_PROJECTS]: (state, { payload }) => ({
     ...state,
     projects: payload
+  }),
+  [SET_CURRENT_PROJECT]: (state, { payload }) => ({
+    ...state,
+    currentProject: payload
   })
 }, initialState);
+
+// selectors
+export const projectsSelector = state => state.repo.projects;
 
 // logics
 export const logics = require('./logics').default;
