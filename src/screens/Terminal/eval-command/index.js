ef67781e00779d938bffcfbb46348af6e05fe695
DT <dark.turtle@protonmail.com>
2017-11-25 10:05:22 +0700

    Compact command arg string

--- src/screens/Terminal/eval-command/index.js
+++ src/screens/Terminal/eval-command/index.js
@@ -3,7 +3,10 @@ import evaluators from './evaluators';
 export default function evalCommand(input, log) {
   const [command, ...args] = input.split(' ');
   if (Object.keys(evaluators).includes(command)) {
-    return evaluators[command]({ log, args });
+    return evaluators[command]({
+      log,
+      args: args.filter(e => e !== '')
+    });
   } else {
     log(`Command not found: ${command}`);
     return true;
