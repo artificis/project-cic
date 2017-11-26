bb5eff8912a71883c580ede68a30ce9d103a58ce
DT <dark.turtle@protonmail.com>
2017-11-26 17:56:06 +0700

    DRY-ize repo logics

--- src/services/repo/logics.js
+++ src/services/repo/logics.js
@@ -1,63 +1,39 @@
 import { createLogic } from 'redux-logic';
-import { gitlabApiClient } from 'services/utils';
+import { withCommonErrorHandling, gitlabApiClient } from 'services/utils';
 import { authTokenSelector } from 'services/auth';
 import { GET_PROJECTS, GET_REPOSITORY_TREE, setProjects, setRepositoryTree } from 'services/repo';
-import { setTerminalBusy, spitToTerminal as log } from 'services/terminal';
+import { spitToTerminal as log } from 'services/terminal';
 
 const projectsLogic = createLogic({
   type: GET_PROJECTS,
-  process: async ({ getState }, dispatch, done) => {
-    try {
-      const api = gitlabApiClient(authTokenSelector(getState()));
-      dispatch(log('Pulling projects...'));
-      const projects = await api.projects.all({ owned: true, order_by: 'path', sort: 'asc' });
-      const simplifiedProjects = [];
-      for (let project of projects) {
-        dispatch(log(project.path));
-        simplifiedProjects.push({
-          id: project.id,
-          path: project.path
-        });
-      }
-      dispatch(setProjects(simplifiedProjects));
-    } catch (err) {
-      if (err.statusCode === 401) {
-        dispatch(log('Error: access token is invalid or expired. Please sign out and sign in again.'));
-      } else {
-        dispatch(log(`Error: ${JSON.stringify(err.getBody())}`));
-      }
-    } finally {
-      dispatch(log('&nbsp;'));
-      dispatch(setTerminalBusy(false));
-      done();
+  process: withCommonErrorHandling(async ({ getState }, dispatch) => {
+    const api = gitlabApiClient(authTokenSelector(getState()));
+    dispatch(log('Pulling projects...'));
+    const projects = await api.projects.all({ owned: true, order_by: 'path', sort: 'asc' });
+    const simplifiedProjects = [];
+    for (let project of projects) {
+      dispatch(log(project.path));
+      simplifiedProjects.push({
+        id: project.id,
+        path: project.path
+      });
     }
-  }
+    dispatch(setProjects(simplifiedProjects));
+  })
 });
 
 const repositoryTreeLogic = createLogic({
   type: GET_REPOSITORY_TREE,
-  process: async ({ getState, action: { payload } }, dispatch, done) => {
+  process: withCommonErrorHandling(async ({ getState, action: { payload } }, dispatch) => {
+    const api = gitlabApiClient(authTokenSelector(getState()));
+    dispatch(log('Pulling repository tree...'));
     const { projectId, repoTreePath } = payload;
-    try {
-      const api = gitlabApiClient(authTokenSelector(getState()));
-      dispatch(log('Pulling repository tree...'));
-      const tree = await api.projects.repository.listTree(projectId, repoTreePath);
-      for (let item of tree) {
-        dispatch(log(item.name));
-      }
-      dispatch(setRepositoryTree(tree));
-    } catch (err) {
-      if (err.statusCode === 401) {
-        dispatch(log('Error: access token is invalid or expired. Please sign out and sign in again.'));
-      } else {
-        dispatch(log(`Error: ${JSON.stringify(err.getBody())}`));
-      }
-    } finally {
-      dispatch(log('&nbsp;'));
-      dispatch(setTerminalBusy(false));
-      done();
+    const tree = await api.projects.repository.listTree(projectId, repoTreePath);
+    for (let item of tree) {
+      dispatch(log(item.name));
     }
-  }
+    dispatch(setRepositoryTree(tree));
+  })
 });
 
 export default [
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,2 +1,22 @@
 import getGitLabApiClient from 'node-gitlab-api';
+import { setTerminalBusy, spitToTerminal as log } from 'services/terminal';
+
 export const gitlabApiClient = oauthToken => getGitLabApiClient({ oauthToken });
+
+export function withCommonErrorHandling(processFunc) {
+  return async (depObj, dispatch, done) => {
+    try {
+      await processFunc(depObj, dispatch);
+    } catch (err) {
+      if (err.statusCode === 401) {
+        dispatch(log('Error: access token is invalid or expired. Please sign out and sign in again.'));
+      } else {
+        dispatch(log(`Error: ${JSON.stringify(err.getBody())}`));
+      }
+    } finally {
+      dispatch(log('&nbsp;'));
+      dispatch(setTerminalBusy(false));
+      done();
+    }
+  }
+};
