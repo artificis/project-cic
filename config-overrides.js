4e30c48bed1d48cdf07a89c2be6666f1fae1418d
Alchemist <artificis@protonmail.ch>
2018-02-14 05:06:38 +0700

    Use .babelrc file

--- config-overrides.js
+++ config-overrides.js
@@ -3,11 +3,12 @@
 const path = require('path');
 
 module.exports = (config, env) => {
-  config.resolve.modules.push(path.resolve('./src'));
+  const c = { ...config };
+  c.resolve.modules.push(path.resolve('./src'));
 
-  const moduleRulesOneOfArrayLength = config.module.rules[1].oneOf.length;
-  config.module.rules[1].oneOf[moduleRulesOneOfArrayLength - 1].exclude.push(/\.scss$/);
-  config.module.rules[1].oneOf.push({
+  const moduleRulesOneOfArrayLength = c.module.rules[1].oneOf.length;
+  c.module.rules[1].oneOf[moduleRulesOneOfArrayLength - 1].exclude.push(/\.scss$/);
+  c.module.rules[1].oneOf.push({
     test: /\.scss$/,
     use: [
       { loader: require.resolve('style-loader') },
@@ -16,7 +17,8 @@ module.exports = (config, env) => {
     ]
   });
 
-  config.module.rules[1].oneOf[1].options.plugins = ['transform-decorators-legacy'];
+  delete c.module.rules[1].oneOf[1].options.babelrc;
+  delete c.module.rules[1].oneOf[1].options.presets;
   
-  return config;
+  return c;
 };
